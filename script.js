3696041fa0c10915d2dbae225fb03ea187bdbffb
Frederick <155497656+federicogzc@users.noreply.github.com>
2024-03-27 15:01:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 let clickCount = 0;
+const pageTitle = document.getElementById("page-title");
 
 document.getElementById("rotating-image").addEventListener("click", function() {
     clickCount++;
     this.style.transition = "transform 0.5s ease";
-    this.style.transform = "rotate(" + (1080 * clickCount) + "deg)";
+    this.style.transform = 'translate(-50%, -50%) rotate(' + (360 * clickCount) + 'deg)';
 
     let buttons = document.querySelectorAll('.corner-button');
     buttons.forEach(button => {
-        if(clickCount % 2 !== 0) { // Mostrar botones
+        if(clickCount % 2 !== 0) { // Mostrar botones y titulo
             button.style.opacity = "1";
             button.style.transform = "translate(0, 0)";
-        } else { // Ocultar botones
+            pageTitle.classList.remove('hidden');
+        } else { // Ocultar botones y titulo
             let id = button.id;
             let transform = "";
             if(id === "top-left") transform = "translate(-100%, -100%)";
@@ -19,6 +21,7 @@ document.getElementById("rotating-image").addEventListener("click", function() {
             if(id === "bottom-right") transform = "translate(100%, 100%)";
             button.style.opacity = "0";
             button.style.transform = transform;
+            pageTitle.classList.add('hidden');
         }
     });
 });
